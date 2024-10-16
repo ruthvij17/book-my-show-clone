d56638cc5aa7064e543628033f5b387befba3dff
ruthvij17 <130194046+ruthvij17@users.noreply.github.com>
2024-10-16 13:55:38 +0000

    Done with day-03

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -13,6 +13,46 @@ const HomePage = () => {
   return (
     <>
       <HeroCarousel />
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 my-3">
+          The Best of Entertainment
+        </h1>
+        <EntertainmentCardSlider />
+      </div>
+      <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+        <PosterSlider
+          title="Recommended Movies"
+          subtitle="List of Recommonded Movies"
+          poster={recommendedMovies}
+          isDark={false}
+        />
+      </div>
+
+      <div className="bg-premier-800 py-12">
+        <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+          <div className="hidden md:flex">
+            <img
+              src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+              alt="rupay"
+              className="w-full h-full"
+            ></img>
+          </div>
+          <PosterSlider
+            title="Premiers"
+            subtitle="Grand new release every friday"
+            poster={premierMovies}
+            isDark={true}
+          />
+        </div>
+        <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+          <PosterSlider
+            title="Online Streaming events"
+            subtitle="Online Streaming events"
+            poster={onlineStreamEvents}
+            isDark={false}
+          />
+        </div>
+      </div>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS and TS files in the src directory
+    "./public/index.html", // Include your main HTML file
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
